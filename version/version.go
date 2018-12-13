12fb4ef1c494dffe8e4eb190767fa915760baf3e
Marcin Matlaszek <mmatlaszek@gmail.com>
2018-12-13 14:09:41 +0100

    Add proper newest version checking

--- version/version.go
+++ version/version.go
@@ -1,6 +1,9 @@
 package version
 
 import (
+	"fmt"
+
+	"github.com/tcnksm/go-latest"
 	"github.com/zendesk/goship/color"
 	"github.com/zendesk/goship/config"
 )
@@ -13,8 +16,15 @@ var (
 // CheckForNewVersion checks for new version
 func CheckForNewVersion() {
 
-	if config.GlobalConfig.Verbose {
-		color.PrintYellow("Checking for newest version temporarily disabled\n")
+	githubTag := &latest.GithubTag{
+		Owner:      "zendesk",
+		Repository: "goship",
+	}
+
+	result, _ := latest.Check(githubTag, VersionNumber)
+
+	if result.Outdated && config.GlobalConfig.Verbose {
+		color.PrintYellow(fmt.Sprintf("Newer version (%s) available! Checkout project repository to upgrade to the newest version.\n", result.Current))
 	}
 	return
 }
